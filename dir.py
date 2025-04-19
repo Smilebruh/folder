4698d38c69826d0697a0e2a6b365defcf2864bd8
Smilebruh <137908300+Smilebruh@users.noreply.github.com>
2025-04-19 11:33:00 -0400

    Add files via upload
    a data struct to scan directory structure

---
+++ dir.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python3
+
+from __future__ import annotations
+import os 
+
+class GeneralTree():
+    def  __init__(self, path: str = "."):
+        self.path: str = path
+        self.folder: list[GeneralTree] = []
+    
+    def add(self, folder: GeneralTree):
+        self.folder.append(folder)
+
+    def make_tree(self, path=".", obj = None):
+        #BFS (Breadth-First-Searching)
+        if not obj:
+            obj = self
+
+        for dir in [i for i in os.listdir(path) if os.path.isdir(os.path.join(path,i))]:
+            tree = GeneralTree(path=os.path.join(path,dir))
+            obj.add(tree)
+            
+            for chdir in [i for i in os.listdir(tree.path) if os.path.isdir(os.path.join(tree.path, i))]:
+                childir = GeneralTree(path=os.path.join(tree.path, chdir))
+                tree.add(childir)
+            
+                obj.make_tree(path=childir.path,obj = childir)
+
+
+    def print_all(self, obj: GeneralTree = None):
+        #DFS (Depth-First-Searching)
+        if not obj:
+            obj = self
+        
+        for folder in obj.folder:
+            print(folder.path)
+            self.print_all(folder)
+            
+
+
+
+x = GeneralTree()
+x.make_tree()
+
+print([i.path for i in x.folder])
+x.print_all()
